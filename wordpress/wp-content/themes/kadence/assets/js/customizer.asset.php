0c7fe4f0e59b1b183fb024aa80d8efee0f5c1930
You <you@example.com>
2020-12-10 23:01:21 +0500

    Initial commit

---
+++ wordpress/wp-content/themes/kadence/assets/js/customizer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-compose', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '863579bcf41ddc8e216615b43e8ce616');
